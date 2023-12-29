300b58493747ccd142bfe3fe266b87c3be58833a
Mridul Kalia <mridulkalia3@gmail.com>
2023-12-29 18:52:49 +0530

    changes

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ app.use(express.json());
 app.use("/api/auth", router);
 app.use("/api/messages", messageRoute);
 
-const server = app.listen(process.env.PORT, () => {
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, () => {
   console.log(`server connected to port ${process.env.PORT}`);
 });
 
